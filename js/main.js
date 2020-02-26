a2fcff640939766204ffca7ff1d4c5b69202e22a
Doug Greening <doug.greening@strategicintelligence5.com>
2020-02-26 10:18:45 -0600

    Test AWS Build Again

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExampleWork from './example-work';
 
-console.log("Hello from main.js");
+console.log("Hello from main.js post modal");
 
 const myWork = [
   {
